a6634449520fec0faed137cda6b42d7695b91a60
Hack Reactor Student <student@hackreactor.com>
2020-01-28 20:04:30 -0600

    added product JSON file

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,81 +1,91 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import axios from 'axios';
-import elementList from './components/elementList.jsx';
-import data from '../dummy_data.js'
-
-
-class App extends React.Component{
-  constructor(props){
+import React from "react";
+import ReactDOM from "react-dom";
+import axios from "axios";
+import elementList from "./components/elementList.jsx";
+import data from "../dummy_data.js";
+import productsJSON from "../../productsJSON.js";
+class App extends React.Component {
+  constructor(props) {
     super(props);
-     this.state = {
-        currentproduct: '',
-        productIdentifier: data
-      }
+    this.state = {
+      currentproduct: "",
+      productIdentifier: data
+    };
 
-      this.sendTransaction = this.sendTransaction.bind(this);
-      this.deleteTransaction = this.deleteTransaction.bind(this);
-      this.handlecurrentproduct = this.handlecurrentproduct.bind(this);
+    this.sendTransaction = this.sendTransaction.bind(this);
+    this.deleteTransaction = this.deleteTransaction.bind(this);
+    this.handlecurrentproduct = this.handlecurrentproduct.bind(this);
   }
-   componentDidMount () {
-    this.getTransaction()
+  componentDidMount() {
+    // axios post request to server/database to insert all items from 'productJSON'
   }
 
-deleteTransaction (event) {
-let id = event.target.value;
-  axios.delete(`/deleteTransaction/${id}`)
-    .then(()=>{
+  deleteTransaction(event) {
+    let id = event.target.value;
+    axios.delete(`/deleteTransaction/${id}`).then(() => {
       return this.getTransaction();
-});
-}
+    });
+  }
 
-getTransaction () {
-axios.get('/').then((response) => {
-  this.setState({transactions: response.data});
-})
-.catch(function (error) {
-  console.log(error);
-})
-}
+  getTransaction() {
+    axios
+      .get("/")
+      .then(response => {
+        this.setState({ transactions: response.data });
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
+  }
 
-sendTransaction () {
+  sendTransaction() {
+    let newTransaction = {
+      currentproduct: this.state.currentproduct
+    };
 
-let newTransaction = {
-  currentproduct: this.state.currentproduct,
-}
+    axios
+      .get("/", {
+        newTransaction: newTransaction
+      })
+      .then(response => {
+        console.log("responded", response);
+        this.setState({ inputData: "" });
+        this.getTransaction();
+      })
+      .catch(function(error) {
+        console.log(error);
+      });
+  }
 
-axios.post('/post', {
-  newTransaction: newTransaction
-})
-.then((response) => {
-  console.log("responded", response)
-    this.setState({inputData:''});
-    this.getTransaction();
-})
-.catch(function(error){
-  console.log(error);
-})
-}
+  handlecurrentproduct(event) {
+    this.setState({
+      currentproduct: event.target.value
+    });
+  }
 
-handlecurrentproduct(event) {
-this.setState({
-  currentproduct: event.target.value
-})
-}
-
-render () {
-return(
-  <div>
-    <h1>CheckoutBox</h1>
-      <h1>Product</h1>
-        <input value={this.state.currentproduct} onChange={this.handlecurrentproduct}></input>
+  render() {
+    return (
+      <div>
+        <h1>CheckoutBox</h1>
+        <h1>Product</h1>
+        <input
+          value={this.state.currentproduct}
+          onChange={this.handlecurrentproduct}
+        ></input>
         <button onClick={this.sendTransaction}>Add Product</button>
         {this.state.productIdentifier.map((place, index) => {
-          return ( <elementList key={index} id={place.id} place={place.productIdentifier} deleteTransaction={this.deleteTransaction}/>)
-        })
-        }
-  </div>
-)
-}}
+          return (
+            <elementList
+              key={index}
+              id={place.id}
+              place={place.productIdentifier}
+              deleteTransaction={this.deleteTransaction}
+            />
+          );
+        })}
+      </div>
+    );
+  }
+}
 
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(<App />, document.getElementById("app"));
